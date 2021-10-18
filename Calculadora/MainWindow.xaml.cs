52504b1f85b85586f3f193a6ff8316919e7d0915
s3mpe <s3mpe@DESKTOP-TSQ52TP>
2021-10-18 21:02:47 +0200

    Modifico para la creación dinámica de controles, pero no esta correcto.

--- Calculadora/MainWindow.xaml.cs
+++ Calculadora/MainWindow.xaml.cs
@@ -23,6 +23,71 @@ namespace Calculadora
         public MainWindow()
         {
             InitializeComponent();
+            Button numero1 = new Button
+            {
+                Content = "1",
+            Style = (Style)this.Resources["botones"]
+            };
+            Button numero2 = new Button
+            {
+                Content = "2",
+                Style = (Style)this.Resources["botones"]
+            };
+            Button numero3 = new Button
+            {
+                Content = "3",
+                Style = (Style)this.Resources["botones"]
+            };
+            Button numero4 = new Button
+            {
+                Content = "4",
+                Style = (Style)this.Resources["botones"]
+            };
+            Button numero5 = new Button
+            {
+                Content = "5",
+                Style = (Style)this.Resources["botones"]
+            };
+            Button numero6 = new Button
+            {
+                Content = "6",
+                Style = (Style)this.Resources["botones"]
+            };
+            Button numero7 = new Button
+            {
+                Content = "7",
+                Style = (Style)this.Resources["botones"]
+            };
+            Button numero8 = new Button
+            {
+                Content = "8",
+                Style = (Style)this.Resources["botones"]
+            };
+            Button numero9 = new Button
+            {
+                Content = "9",
+                Style = (Style)this.Resources["botones"]
+            };
+            Button numero0 = new Button
+            {
+                Content = "0",
+                Style = (Style)this.Resources["botones"]
+            };
+            numero1.Click += Button_Click;
+            numero2.Click += Button_Click;
+            numero3.Click += Button_Click;
+            numero4.Click += Button_Click;
+            numero5.Click += Button_Click;
+            numero6.Click += Button_Click;
+            numero7.Click += Button_Click;
+            numero8.Click += Button_Click;
+            numero9.Click += Button_Click;
+            numero0.Click += Button_Click;
+        }
+
+        private void Numero1_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
